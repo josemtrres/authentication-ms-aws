49f253c04f5d4ccf76ccade120c097aaf7f4db9c
José Miguel Torres <62628810+josemtrres@users.noreply.github.com>
2023-03-31 10:03:21 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,20 +1,14 @@
 import express from 'express';
 import morgan from 'morgan';
-import path from 'path';
 
 import authRoutes from './routes/auth.routes.js'
-import {verifyToken} from './middleware/verify.tkn.js'; // para que front pueda saber si el token es valido durante las sesion
+//import {verifyToken} from './middleware/verify.tkn.js'; // para que front pueda saber si el token es valido durante las sesion
+
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
 
-//app.get('/', function(req,res){
-//    res.sendFile(path.join(__dirname,'../index.html'));
-//});
-
-
-app.use('/api/auth', authRoutes);
-
+app.use('/api/auth', authRoutes);   
 
 export default app;
